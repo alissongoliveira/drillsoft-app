0f32ff0a89c2c4ef7add786d1b0180d0a92cc9ad
Alisson Gabriel <eng.alissongabriel@gmail.com>
2025-06-29 18:06:06 -0300

    redirecionamento automatico para /login ao acessar a rota /

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import LoginPage from "./features/Login/LoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Redirecionamento da raiz para /login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Tela de login */}
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
